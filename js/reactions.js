6405a38815abd2ea4282d4396301e92324546d0a
JasonMFernando <jasonmathewfd@gmail.com>
2026-08-17 22:37:00 +0530

    Added thumb up emoji

--- js/reactions.js
+++ js/reactions.js
@@ -2,7 +2,7 @@
  * Emoji reaction bar + floating animations over the video.
  */
 (function (global) {
-  const EMOJIS = ["❤️", "😂", "😮", "👏", "🍿", "😖", "😭", "⏳", "😡"];
+  const EMOJIS = ["❤️", "😂", "😮", "👏", "🍿", "😖", "😭", "⏳", "😡", "👍"];
   const STORAGE_KEY = "wp-emoji-bar-collapsed";
   const DND_KEY = "wp-dnd-overlays";
 
